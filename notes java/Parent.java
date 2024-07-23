0ae2ebe0eefe309c2e1423a2a5722c9278d53c9f
Vivek kumar <viveka9415@gmail.com>
2024-07-24 01:11:36 +0530

    Fundamental Main Method command Line Argument

---
+++ notes java/Parent.java
@@ -0,0 +1,11 @@
+ class Parent {
+  public static void main(String args[]) {
+    System.out.println("Parent main");
+  }
+}
+
+class Child extends Parent {
+  public static void main(String args[]) {
+    System.out.println("Child main");
+  }
+}
---
+++ notes java/lect13mainFinal.java
@@ -0,0 +1,5 @@
+public class lect13mainFinal {
+  static final synchronized  public void main(String... vivek){
+    System.out.println("Vaild main method");
+  }
+}
